a4362fe4394bd245b43c094ac0bdaec10c84f673
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-08 14:18:52 +0200

    refactor(front): clear main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { AppRouter } from './app/router/AppRouter';
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
         <StrictMode>
-            <AppRouter />
+            <Provider store={store}>
+                <BrowserRouter>
+                    <AppRouter />
+                </BrowserRouter>
+            </Provider>
         </StrictMode>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.js'
-import { store } from './app/store/store.js'
-import './app/style/index.scss'
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import './app/style/index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
