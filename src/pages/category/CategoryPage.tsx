9280631aea801e00e5d95c65a03754e9ad079133
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-12 22:57:50 +0200

    feat: add basic structure for category page

--- src/pages/category/CategoryPage.tsx
+++ src/pages/category/CategoryPage.tsx
@@ -1,9 +1,39 @@
+import { Controls } from '../../features/controls';
+import data from '../../../db.json';
+import { categories } from '../../shared/const/categories';
+import { NavLink } from 'react-router-dom';
+import './category-page.scss';
+
 export const CategoryPage = () => {
-    const category = location.pathname;
+    const category = location.pathname.slice(1);
+    const description = categories[category].description;
 
     return (
-        <div
-            style={{ padding: '300px 400px', fontSize: '24px' }}
-        >{`${category.slice(1)} page`}</div>
+        <>
+            <Controls />
+            <div className="bread-crumbs">
+                <NavLink to="/">Главная</NavLink>
+                {` > ${description}`}
+            </div>
+            <h1>{description}</h1>
+            <div className="filters-adverts">
+                <section className="filters"></section>
+                <section className="adverts">
+                    {data.map((el) => {
+                        return (
+                            <div className="card" key={el.id}>
+                                <img src={el.image} />
+                                <h2>{el.description}</h2>
+                                <p>{el.location}</p>
+                                <h3>{el.price}</h3>
+                                <p>{el.text}</p>
+                                <span>{el.author}</span>
+                                <p>{el.time}</p>
+                            </div>
+                        );
+                    })}
+                </section>
+            </div>
+        </>
     );
 };
